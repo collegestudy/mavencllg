4d607a135286ddf2d6ac5f12a3ff2a69fa1d2218
Yuktaa Sri Addanki <collegestudy.cs@gmail.com>
2022-11-21 12:15:40 +0530

    Demo for maven-jenkins from college

---
+++ src/main/java/sampleproject1/kmit/App.java
@@ -0,0 +1,13 @@
+package sampleproject1.kmit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
